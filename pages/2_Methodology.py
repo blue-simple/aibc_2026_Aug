64de4a7dff816d0ea0e8f447b039648f788efa10
CC-YOGAPRO7\blue_ <caroline_chua@htx.gov.sg>
2026-08-10 22:14:40 +0800

    upd3 10Aug2026 4_Chatbot.py 2_Methodology.py

--- pages/2_Methodology.py
+++ pages/2_Methodology.py
@@ -1,3 +1,12 @@
 # A comprehensive explanation of the data flows and implementation details.
 # A flowchart illustrating the process flow for each use case in the application. Each use case should have its own flowchart.
 # Refer to the sample here for examples of flowcharts and methodology (Slides 13, 14, and 15)
+
+import streamlit as st
+from PIL import Image
+
+st.title("Methodology")
+
+img_path = Path("../data/method.jpg")
+img = Image.open(img_path)
+st.image(img, use_container_width=True)
