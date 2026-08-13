617e83b9a81c323c447bb7c85b92e53a689199cb
CC-YOGAPRO7\blue_ <caroline_chua@htx.gov.sg>
2026-08-13 22:28:21 +0800

    upd9 13Aug2026

--- home.py
+++ home.py
@@ -14,19 +14,20 @@ def home_page():
     st.info("Use the sidebar to navigate to **About Us**, **Methodology**, **Login** or **Chatbot**.")
 
     st.markdown("---")
-    st.markdown(
-        """
-        <div style="background-color:#e6f2ff; padding:15px; border-radius:5px;">
-        <strong>IMPORTANT NOTICE:</strong> This web application is a prototype developed for <strong>educational purposes only</strong>.  
-        The information provided here is <strong>NOT intended for real-world usage</strong> and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
-        <br><br>
-        <strong>Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.</strong>
-        <br><br>
-        Always consult with qualified professionals for accurate and personalised advice.
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    with st.expander("**Disclaimer**", expanded=False):
+        st.markdown(
+            """
+            <div style="background-color:#e6f2ff; padding:15px; border-radius:5px;">
+            <strong>IMPORTANT NOTICE:</strong> This web application is a prototype developed for <strong>educational purposes only</strong>.  
+            The information provided here is <strong>NOT intended for real-world usage</strong> and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+            <br><br>
+            <strong>Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.</strong>
+            <br><br>
+            Always consult with qualified professionals for accurate and personalised advice.
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
 # Run the home page
 home_page()
